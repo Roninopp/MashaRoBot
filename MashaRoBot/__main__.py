6804971f7b71838cc5a1d1470a3912517b14e68e
Akasuki-1 <79846938+Akasuki-1@users.noreply.github.com>
2021-03-27 17:37:05 +0530

    Update __main__.py

--- MashaRoBot/__main__.py
+++ MashaRoBot/__main__.py
@@ -372,13 +372,8 @@ def Masha_about_callback(update: Update, context: CallbackContext):
             ),
         )
     elif query.data == "masha_back":
-        query.message.edit_text(
-                PM_START_TEXT,
-                reply_markup=InlineKeyboardMarkup(buttons),
-                parse_mode=ParseMode.MARKDOWN,
-                timeout=60,
-                disable_web_page_preview=False,
-        )
+        query.message.delete
+                
 
 
 @run_async
